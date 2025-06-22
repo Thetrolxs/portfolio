66b3d71e2909b790fd48737085aca5ffd5238d91
Thetrolxs <moralesharnisch@gmail.com>
2025-06-22 12:08:42 -0400

    feat: add navbar

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from "../ui/navigation-menu";
+import { NavLinks } from "./nav-links";
+
+export default function NavBar() {
+    return (
+        <header className="sticky top-0 z-50 w-full bg-background/90 backdrop-blur border-b shadow-sm">
+            <NavigationMenu>
+                <NavigationMenuList className="flex gap-4">
+                    <NavigationMenuItem className="flex gap-4">
+                        <NavLinks />
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+        </header>
+    );
+}
