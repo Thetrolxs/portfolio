fd25dbad6e3cd094409307214e071377a0bf4688
Thetrolxs <moralesharnisch@gmail.com>
2025-06-22 10:39:03 -0400

    feat: add nav-link

---
+++ src/components/navbar/nav-links.tsx
@@ -0,0 +1,33 @@
+import { Link } from "lucide-react";
+import { NavigationMenuLink } from "../ui/navigation-menu";
+
+
+export function NavLinks() {
+    return(
+        <>
+            <NavigationMenuLink asChild>
+                <Link href="/">Inicio</Link>
+            </NavigationMenuLink>
+
+            <NavigationMenuLink asChild>
+                <Link href="/about">Sobre mí</Link>
+            </NavigationMenuLink>
+
+            <NavigationMenuLink asChild>
+                <Link href="/skill">Habilidades</Link>
+            </NavigationMenuLink>
+            
+            <NavigationMenuLink asChild>
+                <Link href="/project">Proyectos</Link>
+            </NavigationMenuLink>
+
+            <NavigationMenuLink asChild>
+                <Link href="/more">Extra</Link>
+            </NavigationMenuLink>    
+
+            <NavigationMenuLink asChild>
+                <Link href="/contact">Contacto</Link>
+            </NavigationMenuLink>    
+        </>
+    );
+}
